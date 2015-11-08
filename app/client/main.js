c8c9c597b041bc9151cf772e38e8fb95c2f34946
osamah.net.m@gmail.com <osamah.net.m@gmail.com>
2015-11-08 12:53:47 -0700

    added

--- app/client/main.js
+++ app/client/main.js
@@ -2,4 +2,3 @@ var Rx = require('rx');
 var todoComponent = require('./components/todo/todo.js');
 var todoExtendedComponent = require('./components/extendedTodo/todoExtended.js');
 var spinnerComponent = require('./components/spinner/spinner.js');
-
